bf514caf63f72883f02a7170b4a852dbf190b027
SukmaNurSholehah <aja052670@gmail.com>
2025-04-26 22:42:45 +0700

    segitiga

---
+++ src/L_segitiga.java
@@ -0,0 +1,24 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Sukma Nur
+ */
+public class L_segitiga {
+     public static double luasS(int a, int t){
+      return 0.5 *a * t;
+     }
+    public static void main(String[] args) {
+        L_segitiga ml = new L_segitiga();
+        int a = Integer.parseInt(JOptionPane.showInputDialog("Masukkan alas :"));
+        int t = Integer.parseInt(JOptionPane.showInputDialog("Masukkan tinggi :"));
+        double luas = ml.luasS(a, t);
+        JOptionPane.showMessageDialog(null,"Luas Segitiga :" +luas);
+    }
+}
