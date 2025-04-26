d3b5be2ac8532f88e53eff9f17ce5df863310a6b
SukmaNurSholehah <aja052670@gmail.com>
2025-04-26 21:21:04 +0700

    persegiPanjang

---
+++ src/luasPP.java
@@ -0,0 +1,19 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Sukma Nur
+ */
+import javax.swing.JOptionPane;
+public class luasPP {
+    public static void main(String[] args) {
+        luasPP ml = new luasPP();
+        int p = Integer.parseInt(JOptionPane.showInputDialog("Masukkan panjang :"));
+        int l = Integer.parseInt(JOptionPane.showInputDialog("Masukkan lebar :"));
+        double luas = p * l;
+        JOptionPane.showMessageDialog(null,"Luas Persegi Panjang :" +luas);
+    }            
+}
