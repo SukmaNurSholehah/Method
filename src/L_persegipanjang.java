a431cc1fb60c4207451bb3562b00516c0dada116
SukmaNurSholehah <aja052670@gmail.com>
2025-04-26 22:42:28 +0700

    persegipanjang

---
+++ src/L_persegipanjang.java
@@ -0,0 +1,24 @@
+
+import javax.swing.JOptionPane;
+
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+
+/**
+ *
+ * @author Sukma Nur
+ */
+public class L_persegipanjang {
+    public static int luasPP(int p, int l){
+       return p * l;
+    }
+    public static void main(String[] args) {
+        L_persegipanjang ml = new L_persegipanjang();
+        int p = Integer.parseInt(JOptionPane.showInputDialog("Masukkan panjang :"));
+        int l = Integer.parseInt(JOptionPane.showInputDialog("Masukkan lebar :"));
+        double luas = ml.luasPP(p, l);
+        JOptionPane.showMessageDialog(null,"Luas Persegi Panjang :" +luas);
+    }            
+}
